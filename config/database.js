0a474cc49b7d53d88c72699c85fe431eeb859489
Naman1809 <namankukreja01@gmail.com>
2023-11-04 17:45:14 +0530

    backend

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const MONGO_URI =
+  process.env.MONGO_URI;
+  
+  const connectDatabase = async () => {
+    try {
+        const connect = await mongoose.connect(MONGO_URI);
+        // console.log(MONGO_URI)
+        console.log('Mongo connected:', connect.connection.host);
+    } catch (error) {
+        console.log(error)
+        process.exit()
+    }
+}
+
+module.exports = connectDatabase;
