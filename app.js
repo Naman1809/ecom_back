7770ba3d2ddbd8addaf906d47ea9498aa006ac95
Naman1809 <namankukreja01@gmail.com>
2023-12-10 15:20:16 +0530

    changed

--- app.js
+++ app.js
@@ -1,37 +1,43 @@
+
+const path = require("path");
 const express = require("express");
 const app = express();
 const cookieParser = require("cookie-parser");
-const errorMiddleware = require("./middleware/error")
-const bodyParser = require("body-parser");
+const errorMiddleware = require("./middleware/error");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
+const cors = require("cors");
+// app.use(cors());
+
 
 // COnfig
-dotenv.config({path:"backend/config/config.env"})
+dotenv.config({ path: "config/config.env" });
 
 
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb', extended: true}));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 // app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(fileUpload());
 
-
 // Route imports
-const product = require("./routes/productRoute")
-const user = require("./routes/userRoute")
-const order = require("./routes/orderRoute")
-const payment = require("./routes/paymentRoute")
+const product = require("./routes/productRoute");
+const user = require("./routes/userRoute");
+const order = require("./routes/orderRoute");
+const payment = require("./routes/paymentRoute");
 
-
+const corsOptions ={
+    origin: [process.env.FRONTEND_URL], 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+  }
+  app.use(cors(corsOptions));
+// console.log(process.env.FRONTEND_URL)
 app.use("/api/v1", product);
 app.use("/api/v1", user);
 app.use("/api/v1", order);
 app.use("/api/v1", payment);
 
-
-
 // Middleware for errors
 app.use(errorMiddleware);
-module.exports=app;
-
+module.exports = app;
